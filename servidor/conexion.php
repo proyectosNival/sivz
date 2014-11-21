397fa7293732be26fa778bc7782f5a716210d264
Willy Narvaez <willynarvaez6@gmail.com>
2014-11-21 17:33:41 -0500

    cambios base

--- servidor/conexion.php
+++ servidor/conexion.php
@@ -4,8 +4,8 @@
     	$conexion = null;
         try
         {
-         //$conexion = pg_connect("dbname=ddpk3icqarlmm7 host=ec2-54-243-42-236.compute-1.amazonaws.com port=5432 user=eoqnnirloohnjj password=jymXc-eEappNp3PBgzwjKmwbeJ sslmode=require");
-         $conexion = pg_connect("host=localhost dbname=sivz port=5432 user=postgres password=root");
+         $conexion = pg_connect("dbname=daje5fnc2ou1co host=ec2-184-72-238-68.compute-1.amazonaws.com port=5432 user=xtocfivszednnm password=t1gO5WAORLMKWh7iGFJ394MeGW sslmode=require");
+         //$conexion = pg_connect("host=localhost dbname=sivz port=5432 user=postgres password=root");
          if( $conexion == false )
                  throw new Exception( "Error PostgreSQL ".pg_last_error() );
         }
